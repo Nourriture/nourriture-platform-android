d1758669476dfac30c09f333262a6a3515ca0635
Niels Søholm <nm@9la.dk>
2014-10-30 12:18:35 +0800

    Moved data model utility middleware to a separate javascript file

--- modules/models.js
+++ modules/models.js
@@ -3,42 +3,25 @@
  * Mongoose data models for the Android application backend
  */
 
-module.exports = function (mongoose) {
-    // Custom length validator for strings
-    function strLength(max) {
-        return [
-            function(v) {
-                if(v) {
-                    return v.length < max;
-                }
-                return true;
-            },
-            "Only " + max + " characters are allowed"
-        ];
-    }
+var util = require('../models/data_model_middleware');
 
-    // middleware for automatically updating timestamps ("created" and "modified")
-    function updateTimeStamps(next, done) {
-        if (!this.created) this.created = new Date;
-        this.modified = new Date;
-        next(); done();
-    }
+module.exports = function (mongoose) {
 
     // CONSUMER
     var Consumer = mongoose.Schema({
         created: { type: Date, required: true },
         modified: { type: Date, required: true },
         username: { type: String, required: true},
-        email: { type: String, validate: strLength(128), required:true },
-        name: { type: String, validate: strLength(64), required: true },
-        picture: { type: String, validate: strLength(512) },
-        bio: { type: String, validate: strLength(4096) },
-        occupation: { type: String, validate: strLength(128) },
+        email: { type: String, validate: util.strLength(128), required:true },
+        name: { type: String, validate: util.strLength(64), required: true },
+        picture: { type: String, validate: util.strLength(512) },
+        bio: { type: String, validate: util.strLength(4096) },
+        occupation: { type: String, validate: util.strLength(128) },
         gender: { type: Number, min: 0, max: 2 },
         birthday: Date,
-        website: { type: String, validate: strLength(512) }
+        website: { type: String, validate: util.strLength(512) }
     });
-    Consumer.pre('validate', true, updateTimeStamps);
+    Consumer.pre('validate', true, util.updateTimeStamps);
 
     // MOMENT
     var Like = mongoose.Schema( {
@@ -48,19 +31,19 @@ module.exports = function (mongoose) {
     var Comment = mongoose.Schema({
        created: { type: Date, required: true},
        author: { type: mongoose.Schema.Types.ObjectId, ref: "Consumer", required: true},
-       text: { type: String, validate: strLength(1024)},
+       text: { type: String, validate: util.strLength(1024)},
        likes: [Like]
     });
     var Moment = mongoose.Schema({
         created: { type: Date, required: true },
         author: { type: mongoose.Schema.Types.ObjectId, ref: "Consumer", required: true },
         modified: { type: Date, required: true },
-        text: { type: String, validate: strLength(1024) },
+        text: { type: String, validate: util.strLength(1024) },
         subjectID: String,       // NOTE: Referring to recipe, ingredient, gastronomist or company in Nourriture (3rd party)
         likes: [Like],
         comment: [Comment]
     });
-    Moment.pre('validate', true, updateTimeStamps);
+    Moment.pre('validate', true, util.updateTimeStamps);
 
     // RATING
     var Rating = mongoose.Schema({
@@ -70,7 +53,7 @@ module.exports = function (mongoose) {
         difficulty: { type: Number, min: 1, max: 6},
         subjectID: { type: String, required: true}      // NOTE: Referring to recipe, ingredient, gastronomist or company in Nourriture (3rd party)
     });
-    Rating.pre('validate', true, updateTimeStamps);
+    Rating.pre('validate', true, util.updateTimeStamps);
 
 
     // Bind to DB collection names and return on single object
