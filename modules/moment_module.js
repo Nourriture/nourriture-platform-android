af253e379d0001949f486e1b62d1169980eb477d
Niels Søholm <nm@9la.dk>
2014-11-01 18:21:29 +0800

    Added a POST endpoint for adding comments to a specific moment

--- modules/moment_module.js
+++ modules/moment_module.js
@@ -89,4 +89,34 @@ module.exports = function (server, models) {
             }
         });
     });
+
+    // Create - Comment (to moment)
+    server.post('/moment/:id/comment', function (req, res, next) {
+        models.Moment.findOne({ "_id":req.params.id}, function(err, moment) {
+            if(!err) {
+                if(moment) {
+                    req.body.created = new Date;
+                    moment.comments.push(req.body);
+                    moment.save(function (err) {
+                        if (!err) {
+                            res.send(req.body);
+                            next();
+                        } else {
+                            if (err.name == "ValidationError") {
+                                next(new restify.InvalidContentError(err.toString()));
+                            } else {
+                                console.error("Failed to update moment in database:", err);
+                                next(new restify.InternalError("Failed to insert comment due to an unexpected internal error"));
+                            }
+                        }
+                    });
+                } else {
+                    next(new restify.ResourceNotFoundError("No moment found with the given id"));
+                }
+            } else {
+                console.error("Failed to query database for specific moment:", err);
+                next(new restify.InternalError("Failed to insert comment due to an unexpected internal error"));
+            }
+        });
+    });
 };
